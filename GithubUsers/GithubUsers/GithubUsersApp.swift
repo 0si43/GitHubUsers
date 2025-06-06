16c27d0e4259bfaa6cdad9b86fd5e649f49a942d
st43 <45909001+0si43@users.noreply.github.com>
2025-06-06 09:07:34 +0900

    add new project files

---
+++ GithubUsers/GithubUsers/GithubUsersApp.swift
@@ -0,0 +1,17 @@
+//
+//  GithubUsersApp.swift
+//  GithubUsers
+//
+//  Created by Nakajima on 2025/06/06.
+//
+
+import SwiftUI
+
+@main
+struct GithubUsersApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
