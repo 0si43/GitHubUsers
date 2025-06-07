c03ca247588b7d0a474a1c30b5b2829ceb4311ff
st43 <45909001+0si43@users.noreply.github.com>
2025-06-07 18:41:41 +0900

    fix "Github" -> "GitHub"

--- GithubUsers/UserDetailView.swift
+++ GithubUsers/UserDetailView.swift
@@ -1,6 +1,6 @@
 //
 //  UserDetailView.swift
-//  GithubUsers
+//  GitHubUsers
 //
 //  Created by Nakajima on 2025/06/07.
 //
--- GithubUsers/UserRowView.swift
+++ GithubUsers/UserRowView.swift
@@ -1,6 +1,6 @@
 //
 //  UserRowView.swift
-//  GithubUsers
+//  GitHubUsers
 //
 //  Created by Nakajima on 2025/06/07.
 //
