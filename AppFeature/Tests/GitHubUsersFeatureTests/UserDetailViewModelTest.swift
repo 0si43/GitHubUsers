33517ead2d973b475a6e2e9eff219fad98c8eef7
0si43 <symn43@gmail.com>
2025-06-08 19:33:39 +0900

    add UserDetailViewModelTest

---
+++ AppFeature/Tests/GitHubUsersFeatureTests/UserDetailViewModelTest.swift
@@ -0,0 +1,36 @@
+//
+//  Test.swift
+//  GitHubUsersFeature
+//
+//  Created by Nakajima on 2025/06/08.
+//
+
+@testable import GitHubUsersFeature
+import Testing
+
+@MainActor
+struct UserDetailViewModelTest {
+    let gitHubServiceMock: GitHubServiceMock = .init()
+    let userDetailViewModel: UserDetailViewModel
+    
+    init() {
+        userDetailViewModel = UserDetailViewModel(userId: 1, gitHubService: gitHubServiceMock)
+    }
+    
+    @Test func fetchUser() async throws {
+        let user = GitHubUser.mock(id: 1)
+        gitHubServiceMock.stubUser = user
+        await userDetailViewModel.fetchUser()
+        #expect(userDetailViewModel.user == user)
+    }
+
+    @Test func fetchRepositories() async throws {
+        let repositories = [GitHubRepository(name: "", stargazersCount: 1, fork: false, htmlUrl: "")]
+        let user = GitHubUser.mock(id: 1)
+        userDetailViewModel.user = user
+        gitHubServiceMock.stubRepositories = repositories
+        await userDetailViewModel.fetchRepositories()
+        #expect(userDetailViewModel.repositories == repositories)
+    }
+    
+}
