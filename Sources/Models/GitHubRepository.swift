925a11fb65f48774527d2288f5b3d50b8693f007
st43 <45909001+0si43@users.noreply.github.com>
2025-06-08 11:58:55 +0900

    add Package.swift and change directory structure

---
+++ Sources/Models/GitHubRepository.swift
@@ -0,0 +1,15 @@
+//
+//  GitHubRepository.swift
+//  GitHubUsers
+//
+//  Created by Nakajima on 2025/06/07.
+//
+
+public struct GitHubRepository: Codable, Hashable {
+    public let name: String
+    public let language: String?
+    public let stargazersCount: Int
+    public let description: String?
+    public let fork: Bool
+    public let htmlUrl: String
+}
