41a922d60f709bb8e216deb0ddb6a224d1525f6e
st43 <45909001+0si43@users.noreply.github.com>
2025-06-08 19:33:39 +0900

    add UserDetailViewModelTest

--- AppFeature/Sources/Models/GitHubRepository.swift
+++ AppFeature/Sources/Models/GitHubRepository.swift
@@ -12,4 +12,12 @@ public struct GitHubRepository: Codable, Hashable, Sendable {
     public let description: String?
     public let fork: Bool
     public let htmlUrl: String
+    public init(name: String, language: String? = nil, stargazersCount: Int, description: String? = nil, fork: Bool, htmlUrl: String) {
+        self.name = name
+        self.language = language
+        self.stargazersCount = stargazersCount
+        self.description = description
+        self.fork = fork
+        self.htmlUrl = htmlUrl
+    }
 }
