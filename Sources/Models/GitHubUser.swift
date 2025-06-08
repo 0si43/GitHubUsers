dcbe3b1e9f4fb2bced183d8ad7e3b8fe5307842b
0si43 <symn43@gmail.com>
2025-06-08 14:44:33 +0900

    fix GitHubUser parameter

--- Sources/Models/GitHubUser.swift
+++ Sources/Models/GitHubUser.swift
@@ -8,23 +8,23 @@
 public struct GitHubUser: Codable, Hashable, Identifiable {
     public let id: Int
     public let login: String
-    public let name: String?
     public let avatarUrl: String
     public let htmlUrl: String
     // detail information
+    public let name: String?
     public let followers: Int?
     public let following: Int?
-    public init(id: Int, login: String, name: String? = nil, avatarUrl: String, htmlUrl: String, followers: Int? = nil, following: Int? = nil) {
+    public init(id: Int, login: String, avatarUrl: String, htmlUrl: String, name: String? = nil, followers: Int? = nil, following: Int? = nil) {
         self.id = id
         self.login = login
-        self.name = name
         self.avatarUrl = avatarUrl
         self.htmlUrl = htmlUrl
+        self.name = name
         self.followers = followers
         self.following = following
     }
     
     public static func mock(id: Int) -> GitHubUser {
-        .init(id: id, login: "login", name: "name", avatarUrl: "avatarUrl", htmlUrl: "htmlUrl", followers: 100, following: 100)
+        .init(id: id, login: "login", avatarUrl: "avatarUrl", htmlUrl: "htmlUrl", name: "name", followers: 100, following: 100)
     }
 }
